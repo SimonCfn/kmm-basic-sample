fb6acf29210ce71747d424dd6a56fc500c518bdb
SimonCfn <61909875+SimonCfn@users.noreply.github.com>
2023-05-22 17:13:05 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,3 +11,17 @@ rootProject.name = "KmmSample"
 include(":androidApp")
 include(":shared")
 
+gradlePlugin {
+    plugins {
+        id("com.chromaticnoise.multiplatform-swiftpackage") version "2.0.3"
+    }
+}
+
+multiplatformSwiftPackage {
+    swiftToolsVersion("5.3")
+    targetPlatforms {
+      iOS { v("13") }
+    }
+}
+
+packageName("SwiftPackage")
