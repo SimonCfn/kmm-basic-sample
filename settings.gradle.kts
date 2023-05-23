606b91a5ec316b05aa970c6721287d86abe852ad
SimonCfn <61909875+SimonCfn@users.noreply.github.com>
2023-05-23 11:41:09 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,26 +2,6 @@ pluginManagement {
     repositories {
         google()
         gradlePluginPortal()
-        mavenCentral()
     }
 }
-
-rootProject.name = "KmmSample"
-
-include(":androidApp")
-include(":shared")
-
-gradlePlugin {
-    plugins {
-        id("com.chromaticnoise.multiplatform-swiftpackage") version "2.0.3"
-    }
-}
-
-multiplatformSwiftPackage {
-    swiftToolsVersion("5.3")
-    targetPlatforms {
-      iOS { v("13") }
-    }
-}
-
-packageName("SwiftPackage")
+rootProject.name = "MyCardKmm"
