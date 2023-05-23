fb7742aea84d4619e442e023ee6e2bb1f9426818
SimonCfn <61909875+SimonCfn@users.noreply.github.com>
2023-05-23 11:48:05 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,3 @@
-import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
-
-repositories {
-    mavenCentral()
-    google()
-    gradlePluginPortal()
-}
-
 plugins {
     id("com.chromaticnoise.multiplatform-swiftpackage") version "2.0.3"
 }
