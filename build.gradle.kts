26dfb39a1584ba75dbcd063f7c482e7306f010fe
SimonCfn <61909875+SimonCfn@users.noreply.github.com>
2023-05-23 15:29:07 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,18 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
 repositories {
     mavenCentral()
     google()
     gradlePluginPortal()
 }
 
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id("org.jetbrains.kotlin.multiplatform") version "1.9.0-Beta"
-    id("com.chromaticnoise.multiplatform-swiftpackage") version "2.0.3"
+    id("maven-publish")
+    kotlin("plugin.serialization") version "1.8.10"
+    alias(libs.plugins.multiplatform)
+    alias(libs.plugins.multiplatform.spm)
+    alias(libs.plugins.detekt)
 }
 
 multiplatformSwiftPackage {
@@ -17,5 +23,45 @@ multiplatformSwiftPackage {
     }
     outputDirectory(File(projectDir, "/"))
     zipFileName("MyCardKmm_iOS")
+}
+
+kotlin {
+    android {
+    }
+
+    multiplatformSwiftPackage {
+        packageName("MyCardKmm")
+        swiftToolsVersion("5.6")
+        targetPlatforms {
+            iOS { v("14") }
+        }
+        outputDirectory(File(projectDir, "/"))
+        zipFileName("MyCardKmm_iOS")
+    }
+
+    val xcf = XCFramework()
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "MyCardKmm"
+            xcf.add(this)
+        }
+    }
+    
+    val iosX64Main by getting
+    val iosArm64Main by getting
+    val iosSimulatorArm64Main by getting
+    val iosMain by creating {
+        dependsOn(commonMain)
+        iosX64Main.dependsOn(this)
+        iosArm64Main.dependsOn(this)
+        iosSimulatorArm64Main.dependsOn(this)
+        dependencies {
+            implementation(libs.ktor.ios)
+        }
+    }
 }
 
