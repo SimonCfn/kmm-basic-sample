056dafc5e96a291709fcbff832660d53134295aa
SimonCfn <61909875+SimonCfn@users.noreply.github.com>
2023-05-23 10:38:48 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,90 +1,27 @@
-buildscript {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-        classpath("com.android.tools.build:gradle:7.0.4")
-    }
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
+repositories {
+    mavenCentral()
+    google()
+    gradlePluginPortal()
 }
 
-gradlePlugin {
-    plugins {
-        create("pluginMaven") {
-            id = "com.chromaticnoise.multiplatform-swiftpackage"
-            implementationClass = "com.chromaticnoise.multiplatformswiftpackage.MultiplatformSwiftPackagePlugin"
-        }
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+plugins {
+    java
+    kotlin("multiplatform")
+    id("com.github.pagr0m.kotlin.native.spm")
 }
 
 kotlin {
-    multiplatformSwiftPackage {
-        packageName("MyCardKmm")
-        swiftToolsVersion("5.6")
-        targetPlatforms {
-            iOS { v("14") }
-        }
-        outputDirectory(File(projectDir, "/"))
-        zipFileName("MyCardKmm_iOS")
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("pluginMaven") {
-            pom {
-                groupId = "com.chromaticnoise.multiplatform-swiftpackage"
-                artifactId = "com.chromaticnoise.multiplatform-swiftpackage.gradle.plugin"
-
-                name.set("Multiplatform Swift Package")
-                description.set("Gradle plugin to generate a Swift.package file and XCFramework to distribute a Kotlin Multiplatform iOS library")
-                url.set("https://github.com/ge-org/multiplatform-swiftpackage")
-
-                licenses {
-                    license {
-                        name.set("Apache License, Version 2.0")
-                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                developers {
-                    developer {
-                        name.set("Georg Dresler")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:https://github.com/ge-org/multiplatform-swiftpackage.git")
-                    developerConnection.set("scm:git:ssh://git@github.com/ge-org/multiplatform-swiftpackage.git")
-                    url.set("https://github.com/ge-org/multiplatform-swiftpackage")
-                }
+    iosArm64 {
+        binaries {
+            framework {
+                baseName = "KlibIOS"
             }
         }
     }
 
-    repositories {
-        maven {
-            val releasesUrl = "https://ltlocaldev.jfrog.io/artifactory/api/swift/testIOSkmm/"
-            name = "testIOSkmm"
-            credentials {
-                username = System.getenv("SONATYPE_NEXUS_USERNAME")
-                password = System.getenv("SONATYPE_NEXUS_PASSWORD")
-            }
-        }
+    spm {
+        ios("14") { }
     }
-}
-
-signing {
-    sign(publishing.publications["pluginMaven"])
 }
