8febfddc9920a91302898d6db44c86ab3d63e4ff
SimonCfn <61909875+SimonCfn@users.noreply.github.com>
2023-05-23 09:24:06 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,13 @@ buildscript {
     }
 }
 
-plugins {
-  id("com.chromaticnoise.multiplatform-swiftpackage") version "2.0.3"
+gradlePlugin {
+    plugins {
+        create("pluginMaven") {
+            id = "com.chromaticnoise.multiplatform-swiftpackage"
+            implementationClass = "com.chromaticnoise.multiplatformswiftpackage.MultiplatformSwiftPackagePlugin"
+        }
+    }
 }
 
 allprojects {
@@ -23,4 +28,63 @@ allprojects {
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
+}
+
+kotlin {
+    multiplatformSwiftPackage {
+        packageName("MyCardKmm")
+        swiftToolsVersion("5.6")
+        targetPlatforms {
+            iOS { v("14") }
+        }
+        outputDirectory(File(projectDir, "/"))
+        zipFileName("MyCardKmm_iOS")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("pluginMaven") {
+            pom {
+                groupId = "com.chromaticnoise.multiplatform-swiftpackage"
+                artifactId = "com.chromaticnoise.multiplatform-swiftpackage.gradle.plugin"
+
+                name.set("Multiplatform Swift Package")
+                description.set("Gradle plugin to generate a Swift.package file and XCFramework to distribute a Kotlin Multiplatform iOS library")
+                url.set("https://github.com/ge-org/multiplatform-swiftpackage")
+
+                licenses {
+                    license {
+                        name.set("Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("Georg Dresler")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/ge-org/multiplatform-swiftpackage.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/ge-org/multiplatform-swiftpackage.git")
+                    url.set("https://github.com/ge-org/multiplatform-swiftpackage")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            val releasesUrl = "https://ltlocaldev.jfrog.io/artifactory/api/swift/testIOSkmm/"
+            name = "testIOSkmm"
+            credentials {
+                username = System.getenv("SONATYPE_NEXUS_USERNAME")
+                password = System.getenv("SONATYPE_NEXUS_PASSWORD")
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["pluginMaven"])
 }
