f234aa15879fcab7954ab58d5a38d1cef319e4f1
SimonCfn <61909875+SimonCfn@users.noreply.github.com>
2023-05-23 15:14:09 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,16 +9,13 @@ plugins {
     id("com.chromaticnoise.multiplatform-swiftpackage") version "2.0.3"
 }
 
-kotlin {
-    ios()
-    
-    multiplatformSwiftPackage {
-        packageName("MyCardKmm")
-        swiftToolsVersion("5.6")
-        targetPlatforms {
-            iOS { v("14") }
-        }
-        outputDirectory(File(projectDir, "/"))
-        zipFileName("MyCardKmm_iOS")
+multiplatformSwiftPackage {
+    packageName("MyCardKmm")
+    swiftToolsVersion("5.6")
+    targetPlatforms {
+        iOS { v("14") }
     }
+    outputDirectory(File(projectDir, "/"))
+    zipFileName("MyCardKmm_iOS")
 }
+
