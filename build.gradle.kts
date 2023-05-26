01b0a4e35766bc2199c3927dbc5391be4194f33a
SimonCfn <61909875+SimonCfn@users.noreply.github.com>
2023-05-26 12:13:17 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,38 @@
-import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
-
 repositories {
     mavenCentral()
     google()
     gradlePluginPortal()
 }
-
 plugins {
-    id("maven-publish")
-    kotlin("plugin.serialization") version "1.8.10"
-    id("org.jetbrains.kotlin.multiplatform") version "1.9.0-Beta"
+    // For the Github action & iOS, add : version("1.8.10")
+    id("org.jetbrains.kotlin.multiplatform") version ("1.8.10")
+    id("com.android.library") version ("8.0.0")
+    id("org.jetbrains.kotlin.plugin.serialization") version ("1.8.10")
+    // For Android => no version
+
+    /*id("org.jetbrains.kotlin.multiplatform")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.plugin.serialization")*/
+
     id("co.touchlab.faktory.kmmbridge") version "0.3.7"
 }
 
+kmmbridge {
+    mavenPublishArtifacts()
+    spm()
+    frameworkName.set("MyCardKmm")
+}
+
+kotlin {
+    android {
+        publishAllLibraryVariants()
+    }
+    val ios = listOf(iosX64(), iosArm64(), iosSimulatorArm64())
+    configure(ios) {
+        binaries {
+            framework {
+                baseName = "MyCardKmm"
+            }
+        }
+    }
+}
