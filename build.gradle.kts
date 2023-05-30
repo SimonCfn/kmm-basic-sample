7896875b7fe39e152e1475113ef259fc5e18ec47
SimonCfn <61909875+SimonCfn@users.noreply.github.com>
2023-05-30 10:29:11 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,22 @@
-repositories {
-    mavenCentral()
-    google()
-    gradlePluginPortal()
-}
-plugins {
-    // For the Github action & iOS, add : version("1.8.10")
-    id("org.jetbrains.kotlin.multiplatform") version ("1.8.10")
-    id("com.android.library") version ("8.0.0")
-    id("org.jetbrains.kotlin.plugin.serialization") version ("1.8.10")
-    // For Android => no version
-
-    id("co.touchlab.faktory.kmmbridge") version "0.3.7"
-}
-
-kmmbridge {
-    mavenPublishArtifacts()
-    spm()
-    frameworkName.set("KmmSample")
-}
-
-kotlin {
-    val ios = listOf(iosX64(), iosArm64(), iosSimulatorArm64())
-    configure(ios) {
-        binaries {
-            framework {
-                baseName = "KmmSample"
-            }
-        }
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
     }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("com.android.tools.build:gradle:7.0.4")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
