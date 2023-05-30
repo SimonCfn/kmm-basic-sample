ed17e643076f7bfc66ab788593b216124923a7b9
SimonCfn <61909875+SimonCfn@users.noreply.github.com>
2023-05-30 10:31:24 +0200

    Update build.gradle.kts

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,27 +1,61 @@
 plugins {
-    id("com.android.application") version "7.2.2" apply false
-    id("com.android.library") version "7.2.2" apply false
-}
-
-kotlin {
-    ios()
-    
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach {
-        it.binaries.framework {
-            baseName = "shared"
-        }
-    }
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("co.touchlab.faktory.kmmbridge") version "0.3.7"
 }
 
 kmmbridge {
     mavenPublishArtifacts()
-    githubReleaseVersions()
     spm()
-    versionPrefix.set("0.8")
+    frameworkName.set("KmmSample")
+}
+
+kotlin {
+    android()
+
+    val ios = listOf(iosX64(), iosArm64(), iosSimulatorArm64())
+    configure(ios) {
+        binaries {
+            framework {
+                baseName = "KmmSample"
+            }
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val androidMain by getting
+        val androidTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+                implementation("junit:junit:4.13.2")
+            }
+        }
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+        }
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+        val iosTest by creating {
+            dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
+        }
+    }
 }
 
 android {
@@ -32,7 +66,6 @@ android {
         targetSdk = 31
     }
 }
-
 
 
 
